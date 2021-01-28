f89c44081188c580448bfe7ebed84111ed78c5bb
Keviiin18 <kevin.buede@t-online.de>
2021-01-28 15:41:15 +0100

    first commit

---
+++ src/main/java/de/kevin/Main.java
@@ -0,0 +1,17 @@
+package de.kevin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello World");
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+}
